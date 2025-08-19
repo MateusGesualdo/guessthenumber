aaeabc192614d895332d7141683b0269cdaa6cbc
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2025-08-19 12:42:57 -0300

    local data persistence

--- core/index.js
+++ core/index.js
@@ -1,28 +1,34 @@
-let correctNumber, numberOfAttempts, gameIsOver;
+function createGame(saveScore, playerName) {
+  let correctNumber, numberOfAttempts, gameIsOver;
 
-function guess(number) {
-  if (gameIsOver) return "Reinicie para jogar novamente";
+  function guess(number) {
+    if (gameIsOver) return "Reinicie para jogar novamente";
 
-  numberOfAttempts++;
+    numberOfAttempts++;
 
-  if (number === correctNumber) {
-    gameIsOver = true;
-    return `Parabéns, você acertou em ${numberOfAttempts} tentativas!`;
-  } else if (number > correctNumber) {
-    return "Muito alto, tente um número menor";
-  } else if (number < correctNumber) {
-    return "Muito baixo, tente um número maior";
-  } else {
-    numberOfAttempts--;
-    return "Entrada inválida";
+    if (number === correctNumber) {
+      gameIsOver = true;
+      const high_score = saveScore(playerName, numberOfAttempts);
+      return `Parabéns, você acertou em ${numberOfAttempts} tentativas! Seu recorde é de ${high_score}.`;
+    } else if (number > correctNumber) {
+      return "Muito alto, tente um número menor";
+    } else if (number < correctNumber) {
+      return "Muito baixo, tente um número maior";
+    } else {
+      numberOfAttempts--;
+      return "Entrada inválida";
+    }
   }
-}
 
-function start() {
-  correctNumber = Math.floor(101 * Math.random());
-  numberOfAttempts = 0;
-  gameIsOver = false;
-  return "Jogo iniciado"
-}
+  function start(name) {
+    playerName = name;
+    correctNumber = Math.floor(101 * Math.random());
+    numberOfAttempts = 0;
+    gameIsOver = false;
+    return "Jogo iniciado";
+  }
 
-start()
+  start(playerName);
+
+  return { start, guess };
+}
--- web/prompt.js
+++ web/prompt.js
@@ -1,15 +1,21 @@
 let gameIsOn = true;
 let message = "Digite um número de 0 a 100";
+let playerName = prompt("Qual o seu nome?");
+
+alert(`Olá, ${playerName}! Boas vindas ao Adivinhe o Número!`);
+
+const game = createGame(saveScore, playerName);
 
 while (gameIsOn) {
   let playerGuess = prompt(message);
-  message = guess(Number(playerGuess));
+  message = game.guess(Number(playerGuess));
 
   if (message.includes("Parabéns")) {
     const gameIsOn = confirm(`${message} Reiniciar?`);
 
     if (gameIsOn) {
-      start();
+      playerName = prompt("Qual o seu nome?");
+      game.start(playerName);
       message = "Digite um número de 0 a 100";
     } else {
       gameIsOn = false;
