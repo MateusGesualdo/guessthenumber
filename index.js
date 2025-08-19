489f3934c5f1a0d35eef2098289f57de50cc6d7c
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2025-08-19 11:10:57 -0300

    created function to restart the game

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-let correctNumber = Math.floor(101 * Math.random());
-let numberOfAttempts = 0;
+let correctNumber, numberOfAttempts, gameIsOver;
 
 function guess(number) {
+  if (gameIsOver) return "Reinicie para jogar novamente";
+
   numberOfAttempts++;
+
   if (number === correctNumber) {
+    gameIsOver = true;
     return `Parabéns, você acertou em ${numberOfAttempts} tentativas!`;
   } else if (number > correctNumber) {
     return "Muito alto, tente um número menor";
@@ -14,3 +17,11 @@ function guess(number) {
     return "Entrada inválida";
   }
 }
+
+function start() {
+  correctNumber = Math.floor(101 * Math.random());
+  numberOfAttempts = 0;
+  gameIsOver = false;
+}
+
+start()
